f6a5848bb2f42275a31eb20c4d8ea01257130e13
Bamboo Le <trucle@google.com>
2021-09-13 15:39:35 -0400

    samples: create topic model

---
+++ samples/snippets/src/main/java/com/example/contactcenterinsights/CreateIssueModel.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.contactcenterinsights;
+
+// [START contactcenterinsights_create_issue_model]
+
+import com.google.cloud.contactcenterinsights.v1.ContactCenterInsightsClient;
+import com.google.cloud.contactcenterinsights.v1.Conversation;
+import com.google.cloud.contactcenterinsights.v1.IssueModel;
+import com.google.cloud.contactcenterinsights.v1.LocationName;
+
+public class CreateIssueModel {
+
+  public static void main(String[] args) throws java.io.IOException {
+    // TODO(developer): Replace this variable before running the sample.
+    String projectId = "my_project_id";
+
+    createIssueModel(projectId);
+  }
+
+  public static IssueModel createIssueModel(String projectId) throws java.io.IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (ContactCenterInsightsClient client = ContactCenterInsightsClient.create()) {
+      // Construct a parent resource.
+      LocationName parent = LocationName.of(projectId, "us-central1");
+
+      // Construct an issue model.
+      IssueModel issueModel =
+          IssueModel.newBuilder()
+              .setDisplayName("my-model")
+              .setInputDataConfig(IssueModel.InputDataConfig.newBuilder()
+                  .setMedium(Conversation.Medium.CHAT)
+                  .build())
+              .build();
+
+      // Call the Insights client to create an issue model.
+      IssueModel response = client.createIssueModelAsync(parent, issueModel).get();
+      System.out.printf("Created %s%n", response.getName());
+      return response;
+    }
+  }
+}
+
+// [END contactcenterinsights_create_issue_model]
---
+++ samples/snippets/src/test/java/com/example/contactcenterinsights/CreateIssueModelIT.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.contactcenterinsights;
+
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.TestCase.assertNotNull;
+
+import com.google.cloud.contactcenterinsights.v1.ContactCenterInsightsClient;
+import com.google.cloud.contactcenterinsights.v1.IssueModel;
+import com.google.cloud.contactcenterinsights.v1.ListConversationsRequest;
+import com.google.cloud.contactcenterinsights.v1.ListConversationsResponse;
+import com.google.cloud.contactcenterinsights.v1.LocationName;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CreateIssueModelIT {
+
+  private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
+  private static final int MIN_CONVERSATION_COUNT = 10000;
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+  private String issueModelName;
+  private int conversationCount;
+
+  private static void requireEnvVar(String varName) {
+    assertNotNull(String.format(varName), String.format(varName));
+  }
+
+  @BeforeClass
+  public static void checkRequirements() {
+    requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
+    requireEnvVar("GOOGLE_CLOUD_PROJECT");
+  }
+
+  @Before
+  public void setUp() throws java.io.IOException {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+
+    try (ContactCenterInsightsClient client = ContactCenterInsightsClient.create()) {
+      // Check if the project has the minimum number of conversations required to create
+      // an issue model. See https://cloud.google.com/contact-center/insights/docs/topic-model.
+      LocationName parent = LocationName.of(PROJECT_ID, "us-central1");
+      ListConversationsRequest.Builder listRequest = ListConversationsRequest.newBuilder()
+          .setParent(parent.toString())
+          .setPageSize(1000);
+
+      conversationCount = 0;
+      while (conversationCount < MIN_CONVERSATION_COUNT) {
+        ListConversationsResponse listResponse =
+            client.listConversationsCallable().call(listRequest.build());
+
+        if (listResponse.getConversationsCount() == 0) {
+          break;
+        }
+        conversationCount += listResponse.getConversationsCount();
+
+        if (!Strings.isNullOrEmpty(listResponse.getNextPageToken())) {
+          break;
+        }
+        listRequest.setPageToken(listResponse.getNextPageToken());
+      }
+    }
+  }
+
+  @After
+  public void tearDown() throws java.io.IOException {
+    try (ContactCenterInsightsClient client = ContactCenterInsightsClient.create()) {
+      client.deleteIssueModel(issueModelName);
+    }
+    System.setOut(null);
+  }
+
+  @Test
+  public void testCreateIssueModel() throws java.io.IOException {
+    if (conversationCount >= MIN_CONVERSATION_COUNT) {
+      IssueModel issueModel = CreateIssueModel.createIssueModel(PROJECT_ID);
+      issueModelName = issueModel.getName();
+      assertThat(bout.toString()).contains(issueModelName);
+    }
+  }
+}
